76c2ba503873ad4609dbd425040b12ec7f4e8f90
Pedro Cunha <pkunha@gmail.com>
2013-07-09 13:46:50 +0100

    Handly rails aliases

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -1,10 +1,14 @@
 alias r='rbenv local 1.8.7-p358'
 
+alias ss='script/server'
 alias sc='script/console'
 alias sg='script/generate'
 alias sd='script/destroy'
 
-alias migrate='rake db:migrate db:test:prepare'
+alias migrate='be rake db:migrate db:test:prepare'
 
 alias be='bundle exec'
 alias bec='bundle exec cucumber'
+
+alias po='be exec rake smartling:push smartling:pull'
+alias deploy='be cap $(git_branch) deploy'
