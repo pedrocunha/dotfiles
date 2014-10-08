ba76eacfc48f11f1f0be066b467b178a27d0333d
Pedro Cunha <pkunha@gmail.com>
2014-10-08 14:05:44 +0100

    Better $PATH

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,5 @@
-export PATH="$HOME/.rbenv/bin:./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
+export PATH="$HOME/.rbenv/bin:./bin:$HOME/.rbenv/shims:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 export PATH="/usr/local/heroku/bin:$PATH"
+export PATH="/usr/local/share/npm/bin:${PATH}"
+export PATH="/usr/local/bin:$PATH"
