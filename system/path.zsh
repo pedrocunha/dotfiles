ab5b1ddaf416036bd0f7c1099d9075f6dd2a3633
Pedro Cunha <pkunha@gmail.com>
2017-01-10 09:44:40 +0000

    Adds fzf and other bootstrap

--- system/path.zsh
+++ system/path.zsh
@@ -3,3 +3,6 @@ export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 export PATH="/usr/local/heroku/bin:$PATH"
 export PATH="/usr/local/share/npm/bin:${PATH}"
 export PATH="/usr/local/bin:$PATH"
+export PATH="/Applications/Postgres.app/Contents/Versions/9.4/bin/:$PATH"
+export PATH="vendor/texlive/bin/x86_64-darwin:$PATH"
+
