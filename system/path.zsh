077130f3a6134c3fda4aa6be44ee526120d2c8fb
Pedro Cunha <pkunha@gmail.com>
2017-05-08 22:14:37 +0100

    Removes a lot of unused files

--- system/path.zsh
+++ system/path.zsh
@@ -1,8 +1,9 @@
-export PATH="$HOME/.rbenv/bin:./bin:$HOME/.rbenv/shims:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
-export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+export PATH="./bin:$PATH"
+export PATH="$ZSH/bin:$PATH"
+export PATH="/usr/local/sbin:$PATH"
 export PATH="/usr/local/heroku/bin:$PATH"
 export PATH="/usr/local/share/npm/bin:${PATH}"
 export PATH="/usr/local/bin:$PATH"
-export PATH="/Applications/Postgres.app/Contents/Versions/9.4/bin/:$PATH"
-export PATH="vendor/texlive/bin/x86_64-darwin:$PATH"
+export PATH="/Applications/Postgres.app/Contents/Versions/9.6/bin/:$PATH"
 
+export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
