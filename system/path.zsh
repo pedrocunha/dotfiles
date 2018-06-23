8c16312097930ec0de6bb31077407abe0bd4463b
Pedro Cunha <pkunha@gmail.com>
2018-06-23 09:23:15 +0100

    Adds Cargo to PATH

--- system/path.zsh
+++ system/path.zsh
@@ -6,5 +6,6 @@ export PATH="/usr/local/share/npm/bin:${PATH}"
 export PATH="/usr/local/bin:$PATH"
 export PATH="/Applications/Postgres.app/Contents/Versions/9.6/bin/:$PATH"
 export PATH="/Applications/SnowSQL.app/Contents/MacOS/:$PATH"
+export PATH="$HOME/.cargo/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
