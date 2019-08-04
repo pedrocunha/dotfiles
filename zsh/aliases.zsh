448b6399449c2b4cb3cf5f5f7a0d5dc8bd31993e
Pedro Cunha <pkunha@gmail.com>
2019-08-04 16:15:17 +0100

    Centralise aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,13 @@
 alias reload!='. ~/.zshrc'
+
+alias python=python3
+
+# grc overides for ls
+# requires "brew install coreutils"
+if $(gls &>/dev/null)
+then
+  alias ls="gls -F --color"
+  alias l="gls -lAh --color"
+  alias ll="gls -l --color"
+  alias la='gls -A --color'
+fi
