945cb8dfc10d46fd69f2c751b0b7656e725ad4f4
Pedro Cunha <pkunha@gmail.com>
2020-12-24 18:13:27 +0000

    Sets ANDROID development env vars

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -13,3 +13,10 @@ export GOPATH="$HOME/code/go"
 export PATH="$GOPATH/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/git/man:$MANPATH"
+
+# Android
+export ANDROID_SDK_ROOT="$HOME/Library/Android/sdk"
+export PATH="$PATH:$ANDROID_SDK_ROOT/emulator"
+export PATH="$PATH:$ANDROID_SDK_ROOT/tools"
+export PATH="$PATH:$ANDROID_SDK_ROOT/tools/bin"
+export PATH="$PATH:$ANDROID_SDK_ROOT/platform-tools"
