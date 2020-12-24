d00eda1a209e5cf37d8a328e2e3f6946313f6786
Pedro Cunha <pkunha@gmail.com>
2020-12-24 18:17:11 +0000

    Sets postgres bin in path

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -2,7 +2,7 @@ export PATH="./bin:$PATH"
 export PATH="$ZSH/bin:$PATH"
 export PATH="/usr/local/sbin:$PATH"
 export PATH="/usr/local/bin:$PATH"
-export PATH="/Applications/Postgres.app/Contents/Versions/9.6/bin/:$PATH"
+export PATH="/Applications/Postgres.app/Contents/Versions/11/bin:$PATH"
 export PATH="/Applications/SnowSQL.app/Contents/MacOS/:$PATH"
 
 # Rust
